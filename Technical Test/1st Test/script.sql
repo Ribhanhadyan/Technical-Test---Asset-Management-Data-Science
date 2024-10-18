62af658e35489de8e87d1e8f3c8aa52e0970b2cc
MRhadyan <67813960+Ribhanhadyan@users.noreply.github.com>
2024-10-18 14:24:21 +0700

    Test Technical BFI

---
+++ Technical Test/1st Test/script.sql
@@ -0,0 +1,4 @@
+create table #customer (id varchar(255), customer varchar(255))
+ 
+INSERT INTO #customer values ('OFA00000053', 'ABBAS, Mohammad Hasan (a.k.a. ABBAS, Mohammad Hassan; a.k.a. ABBAS, Muhammad), Damascus, Syria; DOB 01 Sep 1964; POB Al Ladhiqiyah, Syria; Gender Male (individual)');
+INSERT INTO #customer values ('OFA00000054', 'ABDULAH, Hassanudin (a.k.a. ABDUL, Hasanudin; a.k.a. ABDULLAH, Hasanudin), Damascus, Syria; DOB 01 Sep 1964; POB Al Ladhiqiyah, Syria; Gender Male (individual)');
